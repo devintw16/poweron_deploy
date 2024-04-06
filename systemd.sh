8dd1db9440fda0618b01ac219526b2d3cce04ee3
Devin Weeraratne <devintw16@gmail.com>
2024-04-06 18:00:52 +1100

    [ADAP-1345] bash script for systemd automation

---
+++ systemd.sh
@@ -0,0 +1,9 @@
+#Repositories are version controlled source code store
+#Public service available is www.github.com
+
+# initialize new local repository
+git init 
+
+
+#Ubuntu package list installed 
+apt list --installed | grep mysql
