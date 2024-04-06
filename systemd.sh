a81d29e7120618d30ecf4363223310d855c258fb
Devin Weeraratne <devintw16@gmail.com>
2024-04-06 18:24:17 +1100

    Updates to systemd.sh

--- systemd.sh
+++ systemd.sh
@@ -1,9 +1,14 @@
 #Repositories are version controlled source code store
-#Public service available is www.github.com
+
 
 # initialize new local repository
 git init 
 
+# add new file to track
+git add <filename>
+
+# commit changes
+git commit -m "<commit message>" 
 
 #Ubuntu package list installed 
 apt list --installed | grep mysql
